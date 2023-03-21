103e8093826043a96ff64385341896032a2edf4e
Bruno Vinícius <vbruno96@gmail.com>
2023-03-21 18:45:17 -0300

    create handle input errro and submit function

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ function toggleHiddenPassword() {
     inputPassword.setAttribute('type', 'password')
   }
 }
+
+function handleLogin(event) {
+  event.preventDefault()
+  const formDataRaw = new FormData(event.target)
+  const formData = Object.fromEntries(formDataRaw)
+
+  alert(`Essa é apenas uma página estática para demonstrar os conhecimentos em html, css e javascript.\nA parte do login não é funcional, mas é possivél visualizar os dados enviados pelo console do navegador!`)
+  console.log(formData)
+  console.log('Se você chegou até aqui significa que é curioso igual a mim.\nCaso tenha conhecimento de alguma vaga de emprego Dev Frontend Jr entre em contato comigo através do email: vbruno96@gmail.com\nSerei muito grato com sua ajuda :)')
+}
